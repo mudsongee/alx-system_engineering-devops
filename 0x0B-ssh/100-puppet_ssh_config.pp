4d7572cb24d7948477fd42c958f2842fe46cd923
mudsongee <mohdahmedjafar@gmail.com>
2023-08-27 16:53:42 -0700

    SSH

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -3,8 +3,10 @@
 
 file { 'etc/ssh/ssh_config':
 	ensure => present,
-	content => "SSH client configuration
-		host*
+	content => "
+	
+		#SSH client configuration
+		Host *
 		IdentityFile ~/.ssh/school
 		PasswordAuthentication no",
 	}
